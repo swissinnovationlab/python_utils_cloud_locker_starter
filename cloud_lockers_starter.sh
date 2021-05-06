1b8635921badce4e82e1b0388f718bda10d559c4
schef <zvanstefan@gmail.com>
2021-05-06 16:22:50 +0200

    added gitpython to dep

--- cloud_lockers_starter.sh
+++ cloud_lockers_starter.sh
@@ -12,6 +12,7 @@ if [ -z "$(command -v pip)" ]; then sudo pacman -S python-pip; fi
 
 echo "Install python dependencies [typer]"
 if [ -z $(python -c "import typer") ]; then pip install --user typer; fi
+if [ -z $(python -c "import git") ]; then pip install --user gitpython; fi
 
 CLOUD_LOCKERS_PATH="~/git/swissinnovationlab/cloud_lockers"
 read -p "Enter CLOUD_LOKERS_PATH [$CLOUD_LOCKERS_PATH]: " path
